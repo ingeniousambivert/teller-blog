0a6b6ef2f13dbb8fd7109395424ebc34a6387a18
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-06-17 13:15:19 +0530

    routes: add new routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const { secureRoute } = require("../auth");
+const {
+  createUser,
+  authenticateUser,
+  getUser,
+  accountManagement,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const router = express.Router();
+
+router.post("/signup", createUser);
+
+router.post("/signin", authenticateUser);
+
+router.post("/account/:type", accountManagement);
+
+router.get("/:id", secureRoute, getUser);
+
+router.patch("/update/:id", secureRoute, updateUser);
+
+router.delete("/delete/:id", secureRoute, deleteUser);
 
 module.exports = router;
