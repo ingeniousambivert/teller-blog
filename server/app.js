44f7f65893c885c6a065daa4fc5dcb26234aa1a1
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-06-17 13:16:31 +0530

    add modules

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const logger = require("morgan");
+require("./config/mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
+const categoriesRouter = require("./routes/categories");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/categories", categoriesRouter);
 
 module.exports = app;
