811e705eea4603df56cfbbf5cc2264805aaaf082
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-06-21 00:08:27 +0530

    state: update user state and thunks

--- client/state/user/thunks.js
+++ client/state/user/thunks.js
@@ -36,6 +36,40 @@ export const getUser = async (id) => {
     }
 };
 
+export const updateData = async (id, body) => {
+    const authHeaders = getAuthHeaders();
+    const response = await client.patch(`/api/users/update/data/${id}`, body, authHeaders);
+    const status = response.status;
+    if (status === 401) {
+        return { status };
+    } else {
+        const data = await response.json();
+        return { status, data };
+    }
+};
+export const updateEmail = async (id, body) => {
+    const authHeaders = getAuthHeaders();
+    const response = await client.patch(`/api/users/update/email/${id}`, body, authHeaders);
+    const status = response.status;
+    if (status === 401) {
+        return { status };
+    } else {
+        const data = await response.json();
+        return { status, data };
+    }
+};
+export const updatePassword = async (id, body) => {
+    const authHeaders = getAuthHeaders();
+    const response = await client.patch(`/api/users/update/password/${id}`, body, authHeaders);
+    const status = response.status;
+    if (status === 401) {
+        return { status };
+    } else {
+        const data = await response.json();
+        return { status, data };
+    }
+};
+
 export const manageAccount = async (type, body) => {
     const response = await client.post(`/api/users/account/${type}`, body);
     const status = response.status;
