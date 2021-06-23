b4a1d49bf529e2dc216f32b56166ff9fc7a2a1dd
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-06-23 18:39:32 +0530

    config: update server configs

--- server/config/index.js
+++ server/config/index.js
@@ -11,12 +11,20 @@ const {
   GMAIL_USERNAME,
   GMAIL_PASSWORD,
   CLIENT_URL,
+  CLOUDINARY_NAME,
+  CLOUDINARY_KEY,
+  CLOUDINARY_SECRET,
 } = process.env;
 
 assert(PORT, "PORT is required");
 assert(MONGO_URI, "MONGO URI is required");
 assert(ACCESS_TOKEN_SECRET, "ACCESS_TOKEN_SECRET is required");
-assert(REFRESH_TOKEN_SECRET, "REFRESH_TOKEN_SECRET is required");
+assert(GMAIL_USERNAME, "GMAIL_USERNAME is required");
+assert(GMAIL_PASSWORD, "GMAIL_PASSWORD is required");
+assert(CLIENT_URL, "CLIENT_URL is required");
+assert(CLOUDINARY_NAME, "CLOUDINARY_NAME is required");
+assert(CLOUDINARY_KEY, "CLOUDINARY_KEY is required");
+assert(CLOUDINARY_SECRET, "CLOUDINARY_SECRET is required");
 
 module.exports = {
   port: PORT,
@@ -26,4 +34,7 @@ module.exports = {
   gmailUsername: GMAIL_USERNAME,
   gmailPassword: GMAIL_PASSWORD,
   clientUrl: CLIENT_URL,
+  cloudinaryName: CLOUDINARY_NAME,
+  cloudinaryKey: CLOUDINARY_KEY,
+  cloudinarySecret: CLOUDINARY_SECRET,
 };
--- server/config/redis.js
+++ server/config/redis.js
@@ -1,10 +1,10 @@
 const redis = require("redis");
-const client = redis.createClient({
+const redisClient = redis.createClient({
   port: 6379,
   host: "127.0.0.1",
 });
 
-client.on("connect", () => {
+redisClient.on("connect", () => {
   console.log("Redis client connected");
 });
 
@@ -12,16 +12,16 @@ client.on("connect", () => {
 //   console.log("Client connected to redis and ready to use...");
 // });
 
-client.on("error", (err) => {
+redisClient.on("error", (err) => {
   console.log(err.message);
 });
 
-client.on("end", () => {
+redisClient.on("end", () => {
   console.log("Redis client disconnected");
 });
 
 process.on("SIGINT", () => {
-  client.quit();
+  redisClient.quit();
 });
 
-module.exports = client;
+module.exports = redisClient;
