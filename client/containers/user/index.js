f6a121169b9965ac4651c21b7cf93511cd8d94e2
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-06-23 18:52:32 +0530

    containers: update user containers

--- client/containers/user/index.js
+++ client/containers/user/index.js
@@ -205,11 +205,36 @@ function PictureModal(props) {
                                 className="border focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm hover:border-red-500 hover:text-red-500 transition duration-300">
                                 Cancel
                             </button>
-                            <button
-                                onClick={removePicture}
-                                className="text-white focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm bg-blue-600 hover:bg-blue-500 transition duration-300">
-                                Confirm
-                            </button>
+                            {loading ? (
+                                <button
+                                    type="button"
+                                    className="text-white focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm bg-blue-600 hover:bg-blue-500 transition duration-300 flex flex-row justify-center items-center space-x-5">
+                                    <svg
+                                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white "
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24">
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                    Removing
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={removePicture}
+                                    className="text-white focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm bg-blue-600 hover:bg-blue-500 transition duration-300">
+                                    Remove
+                                </button>
+                            )}
                         </Modal.Footer>
                     </Modal>
                 )}
@@ -341,11 +366,36 @@ function CoverModal(props) {
                                 className="border focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm hover:border-red-500 hover:text-red-500 transition duration-300">
                                 Cancel
                             </button>
-                            <button
-                                onClick={removeCover}
-                                className="text-white focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm bg-blue-600 hover:bg-blue-500 transition duration-300">
-                                Confirm
-                            </button>
+                            {loading ? (
+                                <button
+                                    type="button"
+                                    className="text-white focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm bg-blue-600 hover:bg-blue-500 transition duration-300 flex flex-row justify-center items-center space-x-5">
+                                    <svg
+                                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white "
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24">
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                    Removing
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={removeCover}
+                                    className="text-white focus:outline-none m-1.5 rounded-3xl px-3 py-2 text-sm bg-blue-600 hover:bg-blue-500 transition duration-300">
+                                    Remove
+                                </button>
+                            )}
                         </Modal.Footer>
                     </Modal>
                 )}
