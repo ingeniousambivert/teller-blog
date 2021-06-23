0d69477d9a65ab62a897301c10d4aa99eb0cc1cf
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-06-23 18:37:40 +0530

    state: update user state and thunks

--- client/state/user/thunks.js
+++ client/state/user/thunks.js
@@ -3,76 +3,140 @@ import client, { getAuthHeaders, getRefreshToken } from "../../utils/client";
 // use react query for posts
 
 export const authenticateUser = async (body) => {
-    const response = await client.post("/api/users/auth/signin", body);
-    const status = response.status;
-    const data = await response.json();
-    return { status, data };
+    try {
+        const response = await client.post("/api/users/auth/signin", body);
+        const status = response.status;
+        const data = await response.json();
+        return { status, data };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
+    }
 };
 
 export const createUser = async (body) => {
-    const response = await client.post("/api/users/auth/signup", body);
-    const status = response.status;
-    const data = await response.json();
-    return { status, data };
+    try {
+        const response = await client.post("/api/users/auth/signup", body);
+        const status = response.status;
+        return { status };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
+    }
 };
 
 export const revokeUserAccess = async () => {
-    const authHeaders = getAuthHeaders();
-    const refreshToken = getRefreshToken();
-    const response = await client.delete("/api/users/auth/signout", authHeaders, { refreshToken });
-    const status = response.status;
-    return { status };
+    try {
+        const authHeaders = getAuthHeaders();
+        const refreshToken = getRefreshToken();
+        const response = await client.delete("/api/users/auth/signout", authHeaders, {
+            refreshToken
+        });
+        const status = response.status;
+        return { status };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
+    }
 };
 
 export const getUser = async (id) => {
-    const authHeaders = getAuthHeaders();
-    const response = await client.get(`/api/users/${id}`, authHeaders);
-    const status = response.status;
-    if (status === 401) {
-        return { status };
-    } else {
-        const data = await response.json();
-        return { status, data };
+    try {
+        const authHeaders = getAuthHeaders();
+        const response = await client.get(`/api/users/${id}`, authHeaders);
+        const status = response.status;
+        if (status === 401) {
+            return { status };
+        } else {
+            const data = await response.json();
+            return { status, data };
+        }
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
     }
 };
 
 export const updateData = async (id, body) => {
-    const authHeaders = getAuthHeaders();
-    const response = await client.patch(`/api/users/update/data/${id}`, body, authHeaders);
-    const status = response.status;
-    if (status === 401) {
-        return { status };
-    } else {
-        const data = await response.json();
-        return { status, data };
+    try {
+        const authHeaders = getAuthHeaders();
+        const response = await client.patch(`/api/users/update/data/${id}`, body, authHeaders);
+        const status = response.status;
+        if (status === 401) {
+            return { status };
+        } else {
+            const data = await response.json();
+            return { status, data };
+        }
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
     }
 };
+
+export const updateMedia = async (id, body) => {
+    try {
+        const authHeaders = getAuthHeaders();
+        const response = await client.patch(
+            `/api/users/update/file/${id}`,
+            body,
+            authHeaders,
+            true
+        );
+        const status = response.status;
+        if (status === 401) {
+            return { status };
+        } else {
+            const data = await response.json();
+            return { status, data };
+        }
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
+    }
+};
+
 export const updateEmail = async (id, body) => {
-    const authHeaders = getAuthHeaders();
-    const response = await client.patch(`/api/users/update/email/${id}`, body, authHeaders);
-    const status = response.status;
-    if (status === 401) {
-        return { status };
-    } else {
-        const data = await response.json();
-        return { status, data };
+    try {
+        const authHeaders = getAuthHeaders();
+        const response = await client.patch(`/api/users/update/email/${id}`, body, authHeaders);
+        const status = response.status;
+        if (status === 401) {
+            return { status };
+        } else {
+            const data = await response.json();
+            return { status, data };
+        }
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
     }
 };
 export const updatePassword = async (id, body) => {
-    const authHeaders = getAuthHeaders();
-    const response = await client.patch(`/api/users/update/password/${id}`, body, authHeaders);
-    const status = response.status;
-    if (status === 401) {
-        return { status };
-    } else {
-        const data = await response.json();
-        return { status, data };
+    try {
+        const authHeaders = getAuthHeaders();
+        const response = await client.patch(`/api/users/update/password/${id}`, body, authHeaders);
+        const status = response.status;
+        if (status === 401) {
+            return { status };
+        } else {
+            const data = await response.json();
+            return { status, data };
+        }
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
     }
 };
 
 export const manageAccount = async (type, body) => {
-    const response = await client.post(`/api/users/account/${type}`, body);
-    const status = response.status;
-    const data = response.json();
-    return { status, data };
+    try {
+        const response = await client.post(`/api/users/account/${type}`, body);
+        const status = response.status;
+        const data = response.json();
+        return { status, data };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, data: { error: "Internal Server Error" } };
+    }
 };
